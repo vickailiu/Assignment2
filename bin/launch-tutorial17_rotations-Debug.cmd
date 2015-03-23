cc6fd7e0c5ffd7de4c2f8e527a80e1aecf8414d4
Kai Liu (Victor) <vic.kai.liu@gmail.com>
2015-03-23 15:34:57 +0800

    bin file

---
+++ bin/launch-tutorial17_rotations-Debug.cmd
@@ -0,0 +1,9 @@
+cd C:/Dev/Playground/Assignment2/tutorial17_rotations/
+set PATH=;%PATH%
+
+if [%1]==[--debugger] (
+	SHIFT
+)
+
+"C:\Dev\Playground\Assignment2\Bin\Debug\tutorial17_rotations.exe"  
+pause
